99769dc8d0f676d7a91dfb5de6b45c76e7ca1d95
Franklin Ross <fhross@gmail.com>
2024-12-19 18:08:58 +1100

    feat: set up dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.html", "!node_modules/"],
+  content: ["./**/*.html", "!./node_modules/"],
+  plugins: [],
+  darkMode: ["variant", ["@container root style(--color-scheme: dark) { & }"]],
   theme: {
     extend: {},
   },
-  plugins: [],
 };
