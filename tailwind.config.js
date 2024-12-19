d054cbbb3d5eafedf5eca87725b462095201a83e
Franklin Ross <fhross@gmail.com>
2024-12-20 10:58:52 +1100

    feat: use Tailwind typography plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./**/*.html", "!./node_modules/"],
-  plugins: [],
+  plugins: [require("@tailwindcss/typography")],
   darkMode: ["variant", ["@container root style(--color-scheme: dark) { & }"]],
   theme: {
     extend: {},
