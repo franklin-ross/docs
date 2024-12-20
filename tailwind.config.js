ce2a1e3a6fa5b4bf7980e23d121d7e7e979a27b4
Franklin Ross <fhross@gmail.com>
2024-12-20 11:40:41 +1100

    feat: some custom prose based colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,13 @@ module.exports = {
   plugins: [require("@tailwindcss/typography")],
   darkMode: ["variant", ["@container root style(--color-scheme: dark) { & }"]],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "prose-body": "var(--tw-prose-body)",
+        "prose-headings": "var(--tw-prose-headings)",
+        "prose-border": "var(--tw-prose-th-borders)",
+        "prose-border-subtle": "var(--tw-prose-td-borders)",
+      },
+    },
   },
 };
